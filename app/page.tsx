711a9a1e382385e71fa880d3dfc186702a3d2780
bben95 <benakabp@gmail.com>
2025-02-20 16:31:47 +0530

    added form component

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-   <div>
-    <h1 className="text-3xl text-brand">
+   <div className="flex-center h-screen bg-amber-100">
+    <h1 className="text-3xl text-brand font-bold">
       StoreIt - The only storage solution you need.
     </h1>
    </div>
